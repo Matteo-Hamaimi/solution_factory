c921e15a3007efe64dd49be34fe0eb80dc99c958
mathispappo <mathispappo@gmail.com>
2023-06-23 11:15:13 +0200

    mateo m a pas du tt aider

--- extension_chrome/script.js
+++ extension_chrome/script.js
@@ -1,3 +1,38 @@
+function onWindowLoad() {
+  var message = document.querySelector("#site-content > div > div:nth-child(1) > div:nth-child(4) > div > div > div > div:nth-child(2) > section > div:nth-child(3) > div > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(1) > span > span")
+
+  chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs) {
+      var activeTab = tabs[0];
+      var activeTabId = activeTab.id;
+
+      return chrome.scripting.executeScript({
+          target: { tabId: activeTabId },
+          // injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
+          func: DOMtoString,
+          // args: ['body']  // you can use this to target what element to get the html for
+      });
+
+  }).then(function (results) {
+      message.innerText = results[0].result;
+  }).catch(function (error) {
+      message.innerText = 'There was an error injecting script : \n' + error.message;
+  });
+}
+
+window.onload = onWindowLoad;
+
+function DOMtoString(selector) {
+  if (selector) {
+      selector = document.querySelector(selector);
+      if (!selector) return "ERROR: querySelector failed to find node"
+  } else {
+      selector = document.documentElement;
+  }
+  return selector.outerHTML;
+}
+
+
+
 // Fonction principale pour extraire les commentaires Airbnb
 function extractAirbnbComments() {
   const commentElements = document.querySelectorAll('.comment-list li p');
